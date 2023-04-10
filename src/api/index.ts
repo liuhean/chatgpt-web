f9f6f7662ee13f0974e20abf508b099dc8f90f47
EMONEY\liuhean <liuhean@emoney.cn>
2023-04-10 10:39:45 +0800

    接入数据成功

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,51 +1,56 @@
-import type { AxiosProgressEvent, GenericAbortSignal } from 'axios'
-import { post } from '@/utils/request'
-import { useSettingStore } from '@/store'
+import type { AxiosProgressEvent, GenericAbortSignal } from "axios";
+import { post } from "@/utils/request";
+import { useSettingStore } from "@/store";
 
 export function fetchChatAPI<T = any>(
-  prompt: string,
-  options?: { conversationId?: string; parentMessageId?: string },
-  signal?: GenericAbortSignal,
+	prompt: string,
+	options?: { conversationId?: string; parentMessageId?: string },
+	signal?: GenericAbortSignal
 ) {
-  return post<T>({
-    url: '/chat',
-    data: { prompt, options },
-    signal,
-  })
+	return post<T>({
+		url: "/chat",
+		data: { prompt, options },
+		signal,
+	});
 }
 
 export function fetchChatConfig<T = any>() {
-  return post<T>({
-    url: '/config',
-  })
+	return post<T>({
+		url: "/config",
+	});
 }
 
-export function fetchChatAPIProcess<T = any>(
-  params: {
-    prompt: string
-    options?: { conversationId?: string; parentMessageId?: string }
-    signal?: GenericAbortSignal
-    onDownloadProgress?: (progressEvent: AxiosProgressEvent) => void },
-) {
-  const settingStore = useSettingStore()
+export function fetchChatAPIProcess<T = any>(params: {
+	prompt: string;
+	options?: { conversationId?: string; parentMessageId?: string };
+	signal?: GenericAbortSignal;
+	onDownloadProgress?: (progressEvent: AxiosProgressEvent) => void;
+}) {
+	// const settingStore = useSettingStore();
 
-  return post<T>({
-    url: '/chat-process',
-    data: { prompt: params.prompt, options: params.options, systemMessage: settingStore.systemMessage },
-    signal: params.signal,
-    onDownloadProgress: params.onDownloadProgress,
-  })
+	return post<T>({
+		url: "/chat/ask",
+		data: {
+			question: params.prompt,
+			options: params.options,
+			// systemMessage: settingStore.systemMessage,
+			conversationId: "",
+			userId: "wangxi",
+		},
+		signal: params.signal,
+		onDownloadProgress: params.onDownloadProgress,
+	});
 }
 
 export function fetchSession<T>() {
-  return post<T>({
-    url: '/session',
-  })
+	return post<T>({
+		url: "/session",
+	});
 }
 
 export function fetchVerify<T>(token: string) {
-  return post<T>({
-    url: '/verify',
-    data: { token },
-  })
+	return post<T>({
+		url: "/verify",
+		data: { token },
+	});
 }
